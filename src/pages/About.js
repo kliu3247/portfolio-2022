7d4eb507d468e77f059c7cf60328328570a8384e
Kelly Liu <kelly_liu@berkeley.edu>
2022-03-29 20:39:51 -0700

    all

---
+++ src/pages/About.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+//import headShot from "../../images/design/design-tags/user-research.svg";
+
+
+const About = () => (
+    <>
+
+    <h1 className = "title-about-me">
+        About Me
+    </h1>
+
+    <h3 className = "about-me-text">
+    I’m Kelly! I am an aspiring product designer who loves to innovate, explore, and empathize. I seek to create designs that are user-centric, creative, and impactful.
+    
+    My specialities lie in the technology space, where I often combine my computer science background and my experiences as a human-centered designer.
+    </h3>
+
+    <h3 className ="recruitment">
+        I am currently seeking design internship roles for Fall 2022!
+    </h3>
+    </>
+);
+
+export default About;
